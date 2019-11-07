2b5be8259c52f2aae1ffc8cd74d4b8edccfec1bd
muyangt <46985242+muyangt@users.noreply.github.com>
2019-11-06 20:11:44 -0500

    Updated hessian, gabor and LBP

--- code/edge_detection.py
+++ code/edge_detection.py
@@ -1,79 +1,73 @@
 import cv2
 import numpy as np
-from scipy import signal, misc
-import matplotlib as plt
+from skimage.feature import local_binary_pattern
+from skimage import filters
 
-img = cv2.imread('HighwayDriving/Train/TrainSeq00/image/TrainSeq00_RGB_Image_0000.png')
+img = cv2.imread('HighwayDriving/Train/TrainSeq10/image/TrainSeq10_RGB_Image_0010.png')
+'''
+LBP for texture abstract
+'''
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+radius = 5
+no_points = 8 * radius
+img_lbp = local_binary_pattern(gray_img, no_points, radius, method='uniform')
+img_lbp = np.abs(img_lbp)
+img_lbp /= img_lbp.max()
+img_lbp = cv2.normalize(img_lbp, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+cv2.imwrite('grad_lbp.png', img_lbp)
+print(img_lbp)
 
-sobel_x = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-sobel_xy = np.array([[0, 1, 2], [-1, 0, 1], [-2, -1, 0]])
-sobel_y = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-sobel_yx = np.array([[-2, -1, 0], [-1, 0, 1], [0, 1, 2]])
+cv2.imshow('img', img)
+cv2.imshow('lbp', img_lbp)
 
-roberts_x = np.array([[-1, 0], [0, 1]])
-roberts_y = np.array([[0, -1], [1, 0]])
-
-prewitt_x = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
-prewitt_xy = np.array([[0, 1, 1], [-1, 0, 1], [-1, -1, 0]])
-prewitt_y = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
-prewitt_yx = np.array([[-1, -1, 0], [-1, 0, 1], [0, 1, 1]])
-
-LoG = np.array([[0, 0, -1, 0, 0],
-                [0, -1, -2, -1, 0],
-                [-1, -2, 16, -2, -1],
-                [0, -1, -2, -1, 0],
-                [0, 0, -1, 0, 0]])
 '''
 Sobel cross-gradient operators
 '''
-grad_x_sobel = signal.convolve2d(gray_img, sobel_x, boundary='symm', mode='same')
-grad_xy_sobel = signal.convolve2d(gray_img, sobel_xy, boundary='symm', mode='same')
-grad_y_sobel = signal.convolve2d(gray_img, sobel_y, boundary='symm', mode='same')
-grad_yx_sobel = signal.convolve2d(gray_img, sobel_yx, boundary='symm', mode='same')
-
-grad_x_sobel[grad_x_sobel < 0] = 0
-grad_xy_sobel[grad_xy_sobel < 0] = 0
-grad_y_sobel[grad_y_sobel < 0] = 0
-grad_yx_sobel[grad_yx_sobel < 0] = 0
-grad_sobel = (grad_x_sobel + grad_xy_sobel + grad_y_sobel + grad_yx_sobel)/4
-grad_sobel = grad_sobel.astype('uint8')
+grad_sobel = filters.sobel(gray_img)
 cv2.imshow('sobel', grad_sobel)
+grad_sobel = cv2.normalize(grad_sobel, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+cv2.imwrite('grad_sobel.png', grad_sobel)
 
 '''
 Prewitt cross-gradient operators
 '''
-grad_x_prewitt = signal.convolve2d(gray_img, prewitt_x, boundary='symm', mode='same')
-grad_xy_prewitt = signal.convolve2d(gray_img, prewitt_xy, boundary='symm', mode='same')
-grad_y_prewitt = signal.convolve2d(gray_img, prewitt_y, boundary='symm', mode='same')
-grad_yx_prewitt = signal.convolve2d(gray_img, prewitt_yx, boundary='symm', mode='same')
-
-grad_x_prewitt[grad_x_prewitt < 0] = 0
-grad_xy_prewitt[grad_xy_prewitt < 0] = 0
-grad_y_prewitt[grad_y_prewitt < 0] = 0
-grad_yx_prewitt[grad_yx_prewitt < 0] = 0
-grad_prewitt = (grad_x_prewitt + grad_xy_prewitt + grad_y_prewitt + grad_yx_prewitt)/4
-grad_prewitt = grad_prewitt.astype('uint8')
+grad_prewitt = filters.prewitt(gray_img)
 cv2.imshow('prewitt', grad_prewitt)
+grad_prewitt = cv2.normalize(grad_prewitt, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+cv2.imwrite('grad_prewitt.png', grad_prewitt)
 
 '''
 roberts cross-gradient operators
 '''
-grad_x_roberts = signal.convolve2d(gray_img, roberts_x, boundary='symm', mode='same')
-grad_y_roberts = signal.convolve2d(gray_img, roberts_y, boundary='symm', mode='same')
-
-grad_x_roberts[grad_x_roberts < 0] = 0
-grad_y_roberts[grad_y_roberts < 0] = 0
-grad_roberts = (grad_x_roberts + grad_y_roberts)
-grad_roberts = grad_roberts.astype('uint8')
+grad_roberts = filters.roberts(gray_img)
 cv2.imshow('roberts', grad_roberts)
+grad_roberts = cv2.normalize(grad_roberts, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+cv2.imwrite('grad_roberts.png', grad_roberts)
+
+'''
+Gabor filter
+'''
+gabor_filter_real, gabor_filter_img = filters.gabor(gray_img, 0.6)
+cv2.imshow('gabor_filter_real', gabor_filter_real)
+cv2.imshow('gabor_filter_img', gabor_filter_img)
+gabor_filter_real = cv2.normalize(gabor_filter_real, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+cv2.imwrite('gabor_filter_real.png', gabor_filter_real)
+cv2.imwrite('gabor_filter_img.png', gabor_filter_img)
+
+'''
+Hessian filter
+'''
+grad_hessian = filters.hessian(img_lbp)
+cv2.imshow('grad_hessian', grad_hessian)
+grad_hessian = cv2.normalize(grad_hessian, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+cv2.imwrite('grad_hessian.png', grad_hessian)
 
 '''
 LoG cross-gradient operators
 '''
-grad_LoG = signal.convolve2d(gray_img, LoG, boundary='symm', mode='same')
-grad_LoG[grad_LoG < 0] = 0
-grad_LoG = grad_LoG.astype('uint8')
+grad_LoG = filters.laplace(gray_img)
 cv2.imshow('LoG', grad_LoG)
+grad_LoG = cv2.normalize(grad_LoG, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+cv2.imwrite('grad_LoG.png', grad_LoG)
 
 cv2.waitKey()
